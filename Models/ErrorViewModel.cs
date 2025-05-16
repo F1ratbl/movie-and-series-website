191bfcf8b5ffc64b22e685502c35102272f083a7
F1ratbl <delimanyak2004@hotmail.com>
2025-05-16 13:58:44 +0300

    Initial commit: ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FilmDiziSitesi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
